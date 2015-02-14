894b016990bd640905a0114c5dbbe43e1dfc9e05
smatty <smatty@mb.pikara.ne.jp>
2015-02-14 16:24:17 +0900

    guardを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home Page" do
 
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -23,7 +25,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,10 +35,23 @@ describe "Static Pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
   end
-  it "should have the right title" do
-    visit '/static_pages/about'
-    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+
+  describe "Contact Page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
   end
+
 
 end
