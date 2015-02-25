ee3bd046555ba48fe3d7f442a1390dace0b86579
smatty <smatty@mb.pikara.ne.jp>
2015-02-26 00:22:58 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -114,4 +114,12 @@ describe User do
 
   end
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
